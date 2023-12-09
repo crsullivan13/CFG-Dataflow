ecc217ca456d7efdd04d47133bb39adf27e71ce8
Connor S <connor.r.sullivan9922@gmail.com>
2023-12-09 14:55:17 -0600

    fix up input

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ bool optionExsits(char** begin, char** end, std::string option) {
     return std::find(begin, end, option) != end;
 }
 
-char* getOptionInput(char** begin, char** end, std::string option) {
+const char* getOptionInput(char** begin, char** end, std::string option) {
     char** itr = std::find(begin, end, option);
-    char* input;
+    const char* input = "\0";
 
+    //Some prepend sorcery
     if ( itr != end && ++itr != end ) {
         input = *itr;
     }
@@ -19,45 +20,44 @@ char* getOptionInput(char** begin, char** end, std::string option) {
 }
 
 int main(int argc, char** argv) {
-    
-    if ( optionExsits(argv, argv + argc, "-h") ) {
-        std::cout << "Get some help\n";
-        std::cout << "Expecting \"./analysis -i <infile.ll> -g <outdir>\" for CFG Inference\n";
-        std::cout << "Expecting \"./analysis -i <infile.ll> -s\" for Static Dataflow\n";
-    } else {
-        bool optionsFail = false;
-
-        char* infile = getOptionInput(argv, argv + argc, "-i");
-
-        if ( *infile == '\0' ) {
-            std::cout << "No infile given\n";
-            optionsFail = true;
-        }
-
-        if ( optionExsits(argv, argv + argc, "-g") ) {
-            char* outdir = getOptionInput(argv, argv + argc, "-g");
-
-            if ( *outdir == '\0' ) {
-                std::cout << "No outdir given, defaulting to \"out\"\n";
-                mkdir("out", 0777);
-            } else {
-                mkdir(outdir, 0777);
-            }
-        }
-
-        bool shouldDoDataflow = optionExsits(argv, argv + argc, "-s");
-
-        if ( !optionsFail ) {
-            //call CFG inference
-
-            if ( shouldDoDataflow ) {
-                //call data flow analysis
-            }
-
+    if ( argc > 1 ) {
+        if ( optionExsits(argv, argv + argc, "-h") ) {
+            std::cout << "Get some help\n";
+            std::cout << "Expecting \"./analysis -i <infile.ll> -g <outdir>\" for CFG Inference\n";
+            std::cout << "Expecting \"./analysis -i <infile.ll> -s\" for Static Dataflow\n";
         } else {
-            std::cout << "Bad command line inputs, use -h option for help\n";
-        }
+            bool optionsFail = false;
 
+            const char* infile = getOptionInput(argv, argv + argc, "-i"); //Input file
+
+            bool shouldOutputGraph = false; //We should only output graphs if the -g option exists
+
+            if ( *infile == '\0' ) {
+                std::cout << "No infile given\n";
+                optionsFail = true;
+            } else if ( optionExsits(argv, argv + argc, "-g") ) { //Output dir
+                shouldOutputGraph = true;
+
+                const char* outdir = getOptionInput(argv, argv + argc, "-g");
+
+                if ( *outdir == '\0' ) {
+                    std::cout << "No outdir given, defaulting to \"out\"\n";
+                    mkdir("out", 0777);
+                } else {
+                    mkdir(outdir, 0777);
+                }
+            }
+
+            bool shouldDoDataflow = optionExsits(argv, argv + argc, "-s"); //Switch for data flow anaylysis
+
+            if ( !optionsFail ) {
+                //Call analysis function, pass in in file/out dir/booleans
+            } else {
+                std::cout << "Bad command line inputs, use -h option for help\n";
+            }
+        }
+    } else {
+        std::cout << "Not enough arguments, use -h for help\n";
     }
 
     return 0;
