f57cc9854eee1ef046d65f50e6929c47e2eadc4d
Connor S <connor.r.sullivan9922@gmail.com>
2023-12-09 19:16:49 -0600

    Fill in simple bbl functions

--- bbl.cpp
+++ bbl.cpp
@@ -1,1 +1,3 @@
 #include "bbl.hpp"
+
+//Might need this for if I want to BBL printing, unused for now
--- bbl.hpp
+++ bbl.hpp
@@ -6,7 +6,14 @@
 
 class Bbl {
 public:
+    //Just learned about these initializer lists, also braced initalization, crazy stuff
     Bbl(int bblNumber, std::vector<int> successors = {}, std::vector<Instruction> instructions = {}) : mBblNumber(bblNumber), mSuccessors(successors), mInstructions(instructions) {}
+
+    void addSuccessor(int id) { mSuccessors.emplace_back(id); };
+    const std::vector<int> getSuccesors() { return mSuccessors; };
+    void addInstruction(Instruction instruction) { mInstructions.emplace_back(instruction); };
+    const std::vector<Instruction> getInstructions() { return mInstructions; };
+    const int getBblNumber() { return mBblNumber; }
 
 private:
     int mBblNumber;
